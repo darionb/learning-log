d322d712e1e60ab807e4307234cc162499b105c4
darionb <50345746+darionb@users.noreply.github.com>
2022-02-06 10:54:04 -0700

    adding topics page and link to base.html

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,5 +7,11 @@ from . import views
 app_name = 'learning_logs'
 urlpatterns = [
     # Home page
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    #duplicate home page
+    path('2', views.index, name = 'index'),
+    #test page
+    path('test/', views.testPage, name = 'test page'),
+    #topics page
+    path('topics/', views.topics, name = 'topics')
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 
+#import models
+from .models import Topic
+from .models import Entry
+
 # Create your views here.
 
 def index(request):
     """The home page for learning_log."""
     return render(request, 'learning_logs/index.html')
+
+def testPage(request):
+    """testing a webpage for data return."""
+    #get element by id where id = 'output'
+    #let innerHTML of element = some data
+    return render(request, 'learning_logs/testPage.html')
+
+def topics(request):
+    """web page top display all current topics"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics }
+    return render (request, 'learning_logs/topics.html', context)
