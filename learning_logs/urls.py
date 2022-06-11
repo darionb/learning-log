8199b5ba609607a3d78060ca498d22281b669744
darionb <50345746+darionb@users.noreply.github.com>
2022-06-10 21:59:30 -0700

    Adding functionality to allow users to add entries

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -13,5 +13,8 @@ urlpatterns = [
     # page for individual topics
     path('topics/<int:topic_id>/', views.topic, name='topic'),
     # page to add topic
-    path('add_topic', views.add_topic, name='add_topic')
+    path('add_topic', views.add_topic, name='add_topic'),
+    # page to add entry
+    path('add_entry/<int:topic_id>', views.add_entry, name='add_entry')
+
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 #import models
 from .models import Topic
 from .models import Entry
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 
@@ -11,18 +11,21 @@ def index(request):
     """The home page for learning_log."""
     return render(request, 'learning_logs/index.html')
 
+
 def topics(request):
     """web page top display all current topics"""
     topics = Topic.objects.order_by('date_added')
-    context = {'topics': topics }
+    context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
 
 def topic(request,topic_id):
     """web page top display a single topic and its related entries"""
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date_added')
-    context = {'topic': topic, 'entries' : entries }
+    context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
 
 def add_topic(request):
     """web form to add new topics"""
@@ -37,3 +40,24 @@ def add_topic(request):
             return redirect('learning_logs:topics')
     context = {'form': form}
     return render(request, 'learning_logs/add_topic.html', context)
+
+
+def add_entry(request,topic_id):
+    """add a new entry for a particular topic"""
+
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # not yet posted data; load form
+        form = EntryForm()
+    else:
+        # post request; process data
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+    context = {'form': form, 'topic': topic}
+    return render(request, 'learning_logs/add_entry.html', context)
+
